55a8bb22eaba4ee54ca9b97bd9fba6707ac90878
Beatriz Varela <beatrizsv19@gmail.com>
2021-09-01 09:32:26 -0300

    restart button

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,55 @@
-var frase = $(".text").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#textSize");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input", function(){
-  var conteudo = campo.val();
 
-  var qtdPalavras = conteudo.split(/\S+/).length - 1;
-  $("#contador-palavras").text(qtdPalavras);
+$(function(){
+  atualizaTamanhoFrase();
+  iniciaContadores();
+  iniciaCronometro();
+  $("#botao-reiniciar").on("click", reiniciaJogo);
+})
 
-  var qtdCaracteres = conteudo.length;
-  $("#contador-caracteres").text(qtdCaracteres);
-});
+function atualizaTamanhoFrase() {
+  var frase = $(".text").text();
+  var numPalavras = frase.split(" ").length;
+  var tamanhoFrase = $("#textSize");
+  tamanhoFrase.text(numPalavras);
+}
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function(){
- var idCronometro = setInterval(function(){
-    tempoRestante--;
-    $("#tempo-digitacao").text(tempoRestante);
-    if(tempoRestante < 1){
-      campo.attr("disabled", true);
-      clearInterval(idCronometro);
-    }
-  }, 1000);
-});
+function iniciaContadores() {
+  campo.on("input", function () {
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+  });
+}
+
+function iniciaCronometro() {
+  var tempoRestante = $("#tempo-digitacao").text();
+  campo.one("focus", function () {
+    $("#botao-reiniciar").attr("disabled", true);
+    var idCronometro = setInterval(function () {
+      tempoRestante--;
+      $("#tempo-digitacao").text(tempoRestante);
+      if (tempoRestante < 1) {
+        campo.attr("disabled", true);
+        clearInterval(idCronometro);
+        $("#botao-reiniciar").attr("disabled", false);
+      }
+    }, 1000);
+  });
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    iniciaCronometro();
+}
+
+
