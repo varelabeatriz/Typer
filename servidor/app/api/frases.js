630f53e916ac364cb00acf5d8c5a6a70631a9bc7
Beatriz Varela <beatrizsv19@gmail.com>
2021-09-10 20:33:56 -0300

    changing game phases

--- servidor/app/api/frases.js
+++ servidor/app/api/frases.js
@@ -1,16 +1,16 @@
 var api = {};
 
 var frases = [
-	{_id: 0, texto:'Alura, Cursos online de tecnologia que reinventam sua carreira.', tempo: 15 },
-	{_id: 1, texto:'Debuggers não consertam erros, apenas os exibem em slow motion.',tempo: 8 },
-	{_id: 2, texto:'Caelum, Ensino e Inovação.', tempo: 5 },
-	{_id: 3, texto:'Existem duas tarefas difíceis na Ciência da Computação: invalidação de cache e nomear as coisas.', tempo: 15 },
-	{_id: 4, texto:'Ciência da computação é tão sobre computadores quanto astronomia é sobre telescópios.', tempo: 15 },
-	{_id: 5, texto:'Na minha máquina funciona.', tempo: 5 },
-	{_id: 6, texto:'Hardware é o que você chuta, software é o que você xinga.', tempo: 12 },
-	{_id: 7, texto:'Software em funcionamento mais que documentação abrangente.', tempo: 10 },
-	{_id: 8, texto:'Iterar é humano, recursão é divino.', tempo: 7},
-	{_id: 9, texto:'Existem três jeitos de desenvolver software. O jeito certo, o jeito errado e o meu jeito, que é igual o jeito errado só que mais rápido.', tempo: 20},
+	{_id: 0, texto:"To be, or not to be: that is the question: Whether ’tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them. To die: to sleep...", tempo: 22 },
+	{_id: 1, texto:'This above all: to thine own self be true, And it must follow, as the night the day, Thou canst not then be false to any man.',tempo: 15 },
+	{_id: 2, texto:'Cowards die many times before their deaths; The valiant never taste of death but once.', tempo: 10 },
+	{_id: 3, texto:'Men at some time are masters of their fates: The fault, dear Brutus, is not in our stars, But in ourselves, that we are underlings.', tempo: 15 },
+	{_id: 4, texto:"What's in a name? That which we call a rose By any other word would smell as sweet...", tempo: 15 },
+	{_id: 5, texto:'Good night, good night! Parting is such sweet sorrow, That I shall say good night till it be morrow.', tempo: 16 },
+	{_id: 6, texto:"All the world's a stage, And all the men and women merely players They have their exits and their entrances; And one man in his time plays many parts..", tempo: 18 },
+	{_id: 7, texto:'The robbed that smiles, steals something from the thief.', tempo: 10 },
+	{_id: 8, texto:'Uneasy lies the head that wears the crown.', tempo: 7},
+	{_id: 9, texto:'All that glitters is not gold.', tempo: 6},
 
 	];
 
